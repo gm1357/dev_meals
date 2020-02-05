9bab97f47d31831e089b3e2b0fb89095e293fe39
Gabriel Machado <g_machado1@hotmail.com>
2020-02-04 23:08:18 -0300

    Implementado drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:dev_meals/screens/tabs_screen.dart';
 import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/filters_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       onUnknownRoute: (settings) =>
           MaterialPageRoute(builder: (ctx) => CategoriesScreen()),
